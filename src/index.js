a4b3db4df6fea64b9cda8c57ae3d0a0a41564a1c
dev-akash-sarker <akashsarker210@gmail.com>
2024-06-08 00:21:42 +0600

    msg with template

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import * as Vue from "vue/dist/vue.esm-bundler.js";
-const app = Vue.createApp({});
+const app = Vue.createApp({
+  // template is a HTML part of our Vue application
+  template: `
+  <h1>Hello World</h1>
+  <div> Hello {{counter}}</div>
+  `,
+  data() {
+    return {
+      msg: "World",
+    };
+  },
+});
 app.mount("#app");
