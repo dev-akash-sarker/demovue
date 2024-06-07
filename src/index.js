38db6bdec1db3204f2bedf371f71e78550a973fc
dev-akash-sarker <akashsarker210@gmail.com>
2024-06-08 00:58:02 +0600

    basic

--- src/index.js
+++ src/index.js
@@ -2,13 +2,26 @@ import * as Vue from "vue/dist/vue.esm-bundler.js";
 const app = Vue.createApp({
   // template is a HTML part of our Vue application
   template: `
-  <h1>Hello World</h1>
-  <div> Hello {{counter}}</div>
+  <div>
+    <button v-on:click="increement">increement</button>
+    <button v-on:click="decreement">decreement</button>
+    <div>{{count}}</div>
+  </div>
+
   `,
   data() {
     return {
       msg: "World",
+      count: 0,
     };
+  },
+  methods: {
+    increement() {
+      this.count += 1;
+    },
+    decreement() {
+      this.count == 0 ? (this.count = 0) : (this.count -= 1);
+    },
   },
 });
 app.mount("#app");
